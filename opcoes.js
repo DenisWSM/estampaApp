3a694ba2632e43271d3546805fb8eb9b1ff479ca
Denis Walter da Silva Moraes <101834778+DenisWSM@users.noreply.github.com>
2024-01-23 07:22:44 -0300

    Add files via upload

---
+++ opcoes.js
@@ -0,0 +1,21 @@
+let camiseta=document.querySelector("#camiseta")
+let stamp=document.querySelector("#estampa")
+
+
+function mudaLaranja () {
+    camiseta.style.fill="orange"
+    
+    stamp.style.animation="aparece 2s"
+}
+
+function mudaPreto () {
+    camiseta.style.fill="#ccc"
+   
+    stamp.style.animation="aparece 2s"
+}
+
+function mudaVermelho () {
+    camiseta.style.fill= "#881607"
+ 
+    stamp.style.animation="aparece 2s"
+}
